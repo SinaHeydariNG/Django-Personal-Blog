59ccb9d3bbc67031f0cd27718b3fae949bb09c22
Sina Heydari <imSinaCoder@gmail.com>
2022-10-14 09:09:56 -0400

    FirstCommit

--- project/settings.py
+++ project/settings.py
@@ -86,11 +86,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'imsina',  
-        'USER': 'root',  
-        'PASSWORD': '6625783Sm',  
-        'HOST': 'localhost',  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
  
